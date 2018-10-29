49e497bb9bb400779436dad80952b61ca59438f4
Leander Seefeld <leanseefeld@gmail.com>
2018-10-30 02:08:32 +0700

    project set up

---
+++ webpack.common.js
@@ -0,0 +1,54 @@
+const path = require('path')
+const data = require('./data')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+module.exports = {
+  entry: [
+    './src/index.ejs',
+    './src/styles/main.scss',
+    './src/index.js'
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      {
+        test: /\.(scss|sass)$/,
+        exclude: /node_modules/,
+        loader: [
+          process.env.NODE_ENV === 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.ejs/,
+        exclude: /node_modules/,
+        use: [
+          'html-loader',
+          {
+            loader: 'ejs-html-loader',
+            options: {
+              data
+            }
+          }
+        ]
+      },
+      { test: /.(png|jpeg|svg)/, exclude: /node_modules/, loader: 'file-loader' }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      template: './src/index.ejs'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    })
+  ]
+}
